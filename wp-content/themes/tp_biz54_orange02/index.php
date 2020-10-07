74a1e2fdd0dfaec161dc99994986ccee01f3d852
mc2 <>
2020-10-07 13:17:10 +0900

    フォルダの整理

--- wp-content/themes/tp_biz54_orange02/index.php
+++ wp-content/themes/tp_biz54_orange02/index.php
@@ -3,11 +3,11 @@
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title><?php bloginfo('name'); ?></title>
+<title>ビジネスサイト・企業向け 無料ホームページテンプレート tp_biz54</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="ここにサイト説明を入れます">
 <meta name="keywords" content="キーワード１,キーワード２,キーワード３,キーワード４,キーワード５">
-<link href="<?php echo get_template_directory_uri(); ?>/css/style.css" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/css/style.css" rel="stylesheet" >
 <script src="js/openclose.js"></script>
 <script src="js/fixmenu_pagetop.js"></script>
 <script src="js/ddmenu_min.js"></script>
@@ -27,19 +27,19 @@
 <div id="container">
 
 <header>
-<h1 id="logo"><a href="index.php"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="SAMPLE COMPANY"></a></h1>
+<h1 id="logo"><a href="index.html"><img src="images/logo.png" alt="SAMPLE COMPANY"></a></h1>
 <!--PC用（901px以上端末）メニュー-->
 <nav id="menubar">
 <ul>
-<li><a href="company.html">Company</a></li>
-<li class="ddmenu-title">Service
+<li><a href="<?php echo get_template_directory_uri(); ?>/company.php">Company</a></li>
+<li class="<?php echo get_template_directory_uri(); ?>/ddmenu-title">Service
 	<ul class="ddmenu">
-	<li><a href="service.html">サービスメニュー</a></li>
-	<li><a href="service.html">サービスメニュー</a></li>
-	<li><a href="service.html">サービスメニュー</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/service.php">サービスメニュー</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/service.php">サービスメニュー</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/service.php">サービスメニュー</a></li>
 	</ul>
 </li>
-<li><a href="recruit.html">Recruit</a></li>
+<li><a href="<?php echo get_template_directory_uri(); ?>/recruit.php">Recruit</a></li>
 <li><a href="contact.html">Contact</a></li>
 </ul>
 </nav>
